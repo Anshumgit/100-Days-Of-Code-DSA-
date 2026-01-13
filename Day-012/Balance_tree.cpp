afdd9100d42f7858df3fc91dcc8c04bd9e199b33
Anshumgit <aanshummankar@gmail.com>
2026-01-13 22:46:03 +0530

    Create Balance_tree.cpp

---
+++ Day-012/Balance_tree.cpp
@@ -0,0 +1,51 @@
+// https://www.geeksforgeeks.org/problems/check-for-balanced-tree/1
+
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor to initialize a new node
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  private:
+    int height(Node* root){
+        if(root == NULL){
+            return 0;
+        }
+        
+        int left = height(root -> left);
+        int right = height(root -> right);
+        
+        int ans = max(left,right) + 1;
+        return ans;
+    }
+  public:
+    bool isBalanced(Node* root) {
+        // code here
+        if(root == NULL){
+            return true;
+        }
+        
+        bool left = isBalanced(root -> left);
+        bool right = isBalanced(root -> right);
+        
+        bool diff = abs (height(root -> left) - height(root -> right)) <= 1;
+        
+        if(left && right && diff){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
