58da32f302c981b1acd4bcc5df0b19f19aaee891
Anshumgit <aanshummankar@gmail.com>
2026-01-17 23:16:32 +0530

    Create Left_View_Of_BT.cpp

---
+++ Day-016/Left_View_Of_BT.cpp
@@ -0,0 +1,38 @@
+// https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+    void solve(Node *root , vector<int> &ans , int level){
+        if(root == NULL){
+            return;
+        }
+        if(level == ans.size()){
+            ans.push_back(root -> data);
+        }
+        solve(root -> left , ans , level +1);
+        solve(root -> right , ans , level +1);
+        
+    }    
+    
+  public:
+    vector<int> leftView(Node *root) {
+        // code here
+        vector<int> ans;
+        solve(root , ans,0);
+        return ans;
+    }
+};
