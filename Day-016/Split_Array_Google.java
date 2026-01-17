10f2f7bab94e6007e987dca9f9dde5899fab5c5d
Anshumgit <aanshummankar@gmail.com>
2026-01-17 16:45:46 +0530

    Create Split_Array_Google.java

---
+++ Day-016/Split_Array_Google.java
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/split-array-largest-sum/
+
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int start = 0 ;
+        int end  = 0;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            start = Math.max(start, nums[i]); // gives the max elements in the array
+            end += nums[i]; 
+        }
+
+        while(start < end){
+            int mid = start +(end - start)/2;
+            int sum = 0;
+            int pieces = 1 ;
+
+            for(int num : nums){
+                if(sum + num > mid){
+                    sum = num;
+                    pieces++;
+                }
+                else{
+                    sum += num;
+                }
+            }
+
+            if(pieces > k){
+                start = mid +1 ;
+            }
+            else{
+                end = mid;
+            }
+
+        }
+
+        return end; // or start both will be the same
+
+    }
+}
