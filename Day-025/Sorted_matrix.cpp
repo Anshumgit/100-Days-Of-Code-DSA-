fd8154b8b0282254a54ee9d7908b5e38f3495ca8
Anshumgit <aanshummankar@gmail.com>
2026-01-26 22:18:50 +0530

    Create Sorted_matrix.cpp

---
+++ Day-025/Sorted_matrix.cpp
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/problems/sorted-matrix2333/1
+
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+        // code here
+        vector<int> ans;
+        for(int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < N ; j++){
+                ans.push_back(Mat[i][j]);
+            }
+        }
+        sort(ans.begin() , ans.end());
+        
+        int k = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                Mat[i][j] = ans[k++];
+            }
+        }
+        return Mat;
+    }
+};
