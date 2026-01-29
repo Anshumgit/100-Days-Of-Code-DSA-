b1705775e9a5ae9af38bd19298f67e67b85e35a9
Anshumgit <aanshummankar@gmail.com>
2026-01-29 22:37:40 +0530

    Create Find_the_Duplicate_Number.java

---
+++ Day-028/Find_the_Duplicate_Number.java
@@ -0,0 +1,32 @@
+// same as the 448 
+// can be done by fast and slow pointer approach
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution{
+    public int findDuplicate(int[] nums){
+        int i = 0;
+        while(i < nums.length){
+            if(nums[i] != i+1){
+                int correct = nums[i] - 1;
+                if(nums[i] != nums[correct]){
+                    swap(nums , i , correct);
+                }
+                else{
+                    return nums[i];
+                }
+            }
+            else{
+                i++;
+            }
+            
+        }
+        return -1;
+    }
+
+    public void swap(int[] arr,int a , int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+}
