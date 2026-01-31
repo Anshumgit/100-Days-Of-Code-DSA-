f9d8492dc2864f37e2e49f63ccd1607c2c439793
Anshumgit <aanshummankar@gmail.com>
2026-01-31 22:06:52 +0530

    Create Number_of_digit.java

---
+++ Day-030/Number_of_digit.java
@@ -0,0 +1,9 @@
+public class number_of_digit {
+    public static void main(String[] args) {
+        int n = 10;
+        int base = 2;
+
+        int ans = (int)(Math.log(n) / Math.log(base)) + 1;
+        System.out.println(ans);
+    }
+}
