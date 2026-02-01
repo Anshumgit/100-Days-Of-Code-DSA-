f877524693824cf4829d4bfb47118324be0daab7
Anshumgit <aanshummankar@gmail.com>
2026-02-01 22:38:25 +0530

    Create Power.java

---
+++ Day-031/Power.java
@@ -0,0 +1,16 @@
+public class Power {
+    public static void main(String[] args) {
+        int power = 4;
+        int base = 2;
+        int ans = 1;
+        while(power > 0){
+            if((power & 1) == 1){
+                ans = ans * base;
+            }
+            base = base * base;
+            power = power >> 1;
+
+        }
+        System.out.println(ans);
+    }
+}
