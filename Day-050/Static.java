bffef82480bd7da7b084c39c7de06881228728c6
Anshumgit <aanshummankar@gmail.com>
2026-02-20 22:37:54 +0530

    Update Static.java

--- Day-050/Static.java
+++ Day-050/Static.java
@@ -26,14 +26,14 @@ public class Main {
     public static void main(String[] args) {
         Human Anshum = new Human(19,"Anshum Mankar" , 10000 , false);
         System.out.println("Human 1 added "+Anshum.name);
-        System.out.println("Population Count:"+Anshum.population);
+        System.out.println("Population Count:"+Human.population);
 
         Human Rahul = new Human(19,"Rahul Bhandare" , 15000 , false);
         System.out.println("Human 2 added "+Rahul.name);
-        System.out.println("Population Count:"+Rahul.population);
+        System.out.println("Population Count:"+Human.population);
 
         Human Vikas = new Human(19,"Vikas Choudhary" , 20000 , false);
         System.out.println("Human 3 added "+Vikas.name);
-        System.out.println("Population Count:"+Vikas.population);
+        System.out.println("Population Count:"+Human.population);
     }
 }
