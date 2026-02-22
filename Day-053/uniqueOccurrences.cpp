de9081482b2c70c50066e28230bdaa2f44481f43
Anshumgit <aanshummankar@gmail.com>
2026-02-23 01:04:34 +0530

    Create uniqueOccurrences.cpp

---
+++ Day-053/uniqueOccurrences.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/unique-number-of-occurrences/
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int , int > freq;
+        for(auto x : arr){
+            freq[x]++;
+        }
+
+        unordered_set<int>s;
+        for(auto x : freq){
+            s.insert(x.second);
+        }
+        return freq.size() == s.size();
+    }
+};
