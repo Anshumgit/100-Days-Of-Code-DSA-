06766a89db1723cdb8ff452cd52683a21a38e4c8
Anshumgit <aanshummankar@gmail.com>
2026-02-26 14:57:53 +0530

    Create InstanceOf.java

---
+++ Day-056/InstanceOf.java
@@ -0,0 +1,11 @@
+package OOP4;
+
+public class InstanceOf extends Dot_equal {
+
+    public static void main(String[] args) {
+        InstanceOf obj1 = new InstanceOf();
+        System.out.println(obj1 instanceof InstanceOf);
+        System.out.println(obj1 instanceof Dot_equal);
+
+    }
+}
