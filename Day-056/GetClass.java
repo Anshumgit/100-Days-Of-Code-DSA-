d8becb58a004a2003d63117e02c8c1a199cc03e6
Anshumgit <aanshummankar@gmail.com>
2026-02-26 14:59:51 +0530

    Create GetClass.java

---
+++ Day-056/GetClass.java
@@ -0,0 +1,9 @@
+package OOP4;
+
+public class GetClass {
+    public static void main(String[] args) {
+        GetClass obj1 = new GetClass();
+        System.out.println(obj1.getClass());
+        System.out.println(obj1.getClass().getName());
+    }
+}
