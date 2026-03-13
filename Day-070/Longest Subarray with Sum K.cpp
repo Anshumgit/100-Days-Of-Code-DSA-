d2a2605163530bcd8a0df3e4df8198db60474e32
Anshumgit <aanshummankar@gmail.com>
2026-03-13 23:49:53 +0530

    Create Longest Subarray with Sum K.cpp

---
+++ Day-070/Longest Subarray with Sum K.cpp
@@ -0,0 +1,28 @@
+//https://www.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1
+
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        // code here
+        int n= arr.size();
+        map<long long , int > preSumMap;
+        long long sum = 0;
+        int maxLen = 0;
+        for(int i = 0 ; i < n ; i++){
+            sum += arr[i];
+            if(sum==k){
+                maxLen = max(maxLen , i+1);
+            }
+            long long rem = sum - k;
+            if(preSumMap.find(rem) != preSumMap.end()){
+                int len = i - preSumMap[rem];
+                maxLen = max(maxLen , len);
+            }
+            if(preSumMap.find(sum) == preSumMap.end()){
+                preSumMap[sum] = i;
+            }
+        }
+        
+        return maxLen;
+    }
+};
