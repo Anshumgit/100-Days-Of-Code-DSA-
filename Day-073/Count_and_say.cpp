f5a4ca330a236f2cda4b1a2ba3950afb27a71ed0
Anshumgit <aanshummankar@gmail.com>
2026-03-16 21:53:24 +0530

    Create Count_and_say.cpp

---
+++ Day-073/Count_and_say.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/count-and-say/
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1){
+            return "1";
+        }
+
+        // string say = countAndSay(n-1);
+        // string result = "";
+
+        // for(int i = 0 ; i < say.length(); i++){
+        //     int count = 1;
+        //     char ch = say[i];
+        //     while(i < say.length()-1 && say[i] == say[i+1]){
+        //         count++;
+        //         i++;
+        //     }
+        //     result += to_string(count) + string(1,ch);
+        // }
+        // return result;
+        string say = countAndSay(n-1);
+        string result = "";
+
+        for(int i = 0 ; i < say.length() ; i++){
+            int cnt = 1;
+            char ch = say[i];
+            while(i < say.length()-1 && say[i]== say[i+1]){
+                cnt++;
+                i++;
+            }
+            result += to_string(cnt) + string(1,ch);
+        }
+        return result;
+    }
+};
