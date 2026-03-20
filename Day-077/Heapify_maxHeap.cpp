89fb83114133450e4cf266021e1af2a7e1b4ac18
Anshumgit <aanshummankar@gmail.com>
2026-03-20 20:07:43 +0530

    Create Heapify_maxHeap.cpp

---
+++ Day-077/Heapify_maxHeap.cpp
@@ -0,0 +1,31 @@
+// T.C = O(log n)
+void heapify(int arr[] , int n , int i){
+	int largest = i;
+	int leftChild = 2 * i;
+	int rightChild = 2 * i + 1;
+			
+	if(leftChild < n && arr[largest] < arr[leftChild]){
+		largest = leftChild;
+	}
+	if(rightChild < n && arr[largest] < arr[rightChild]){
+		largest = rightChild;
+	}
+			
+	if(largest != i){
+		swap(arr[i] , arr[largest]);
+		heapify(arr , n , largest);
+	}
+}
+ 
+int main(){
+	int arr[6] = {-1 , 54 , 53 , 55 , 52 , 50};
+	int n = 5;
+	for(int i = n/2 ; i > 0; i--){
+		heapify(arr , n , i);
+	}
+	
+	cout << "Printing array :" << endl;
+	for(int i = 1 ; i <= n ; i++){
+		cout << arr[i] << " ";
+	}cout << endl;
+}
