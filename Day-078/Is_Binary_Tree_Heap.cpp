078354af03458bd6095688299e67dd166a33ccb2
Anshumgit <aanshummankar@gmail.com>
2026-03-21 22:35:12 +0530

    Create Is_Binary_Tree_Heap.cpp

---
+++ Day-078/Is_Binary_Tree_Heap.cpp
@@ -0,0 +1,68 @@
+//https://www.geeksforgeeks.org/problems/is-binary-tree-heap/1
+// TC = O(N);
+// SC = O(N);
+/*
+class Node {
+   public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int countNode(Node* root){
+        if(root == NULL){
+            return 0;
+        }
+        int ans = 1 + countNode(root -> left) + countNode(root -> right);
+        return ans;
+    } 
+    
+    bool isCBT(Node* root , int index , int cnt){
+        if(root == NULL){
+            return true;
+        }
+        if(index >= cnt){
+            return false;
+        }
+        else{
+            bool left = isCBT(root -> left , 2 * index + 1,cnt);
+            bool right = isCBT(root -> right , 2 * index + 2,cnt);
+            return (left && right);
+        }
+    }
+    
+    bool isMaxOrder(Node * root){
+        if(root == NULL){
+            return true;
+        }
+        if(root -> left == NULL && root -> right == NULL){
+            return true;
+        }
+        else if(root -> right == NULL){
+            return root -> data > root -> left -> data;
+        }
+        else if(root -> left != NULL && root -> right != NULL){
+            return (root -> data > root -> left -> data) && (root -> data > root -> right -> data) && isMaxOrder(root -> left) && isMaxOrder(root -> right);
+        }
+    }
+    
+    bool isHeap(Node* tree) {
+        // code here
+        int index = 0;
+        int totalCount = countNode(tree);
+        if(isCBT(tree , index , totalCount) && isMaxOrder(tree)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
