8b7ad08594b2682b3e7b9168915642f157b33135
Anshumgit <aanshummankar@gmail.com>
2026-03-28 23:31:36 +0530

    Create Trie_insert.cpp

---
+++ Day-085/Trie_insert.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+class TrieNode{
+	public:
+		char data;
+		TrieNode* childern[26];
+		bool isTerminal;
+		
+		TrieNode(char ch){
+			this-> data = ch;
+			for(int i = 0 ; i < 26 ; i++){
+				childern[i] = NULL;
+			}
+			isTerminal = false;
+		}
+};
+
+class Trie{
+	public:
+		TrieNode* root; // root node bangai
+		
+		Trie(){
+			root = new TrieNode('\0');
+		}
+		
+		void insertUtil(TrieNode* root , string word){
+			if(word.length() == 0){
+				root -> isTerminal = true;
+				return;
+			}
+			
+			// assumption all letters is in caps
+			int index = word[0] - 'A';
+			TrieNode* child;
+			
+			//present
+			if(root -> childern[index] != NULL){
+				child = root ->childern[index];
+			}
+			else{//absent
+				child = new TrieNode(word[0]);
+				root -> childern[index] = child;
+			}
+			
+			//recursion
+			insertUtil(root , word.substr(1));
+			
+		}
+		
+		void insertWord(string word){
+			insertUtil(root , word);
+		}
+};
+
+int main(){
+	
+	//case1 
+	Trie* t = new Trie();
+	t -> insertWord("abcd");
+	
+	
+	
+}
