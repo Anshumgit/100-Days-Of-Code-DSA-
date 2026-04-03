5a4207fda86648d67ecd655f903c0003760cbf87
Anshumgit <aanshummankar@gmail.com>
2026-04-03 23:13:22 +0530

    Create The N - Queens Puzzle.cpp

---
+++ Day-091/The N - Queens Puzzle.cpp
@@ -0,0 +1,80 @@
+https://www.naukri.com/code360/problems/the-n-queens-puzzle_981286?leftPanelTabValue=SUBMISSION
+
+#include <bits/stdc++.h> 
+
+void addSolution(vector< vector<int> >&ans ,vector< vector<int> > &board,int n ){
+	vector<int> temp;
+	for(int i = 0 ; i < n ; i++){
+		for(int j = 0 ; j < n ; j++){
+			temp.push_back(board[i][j]);
+		}
+	}
+	ans.push_back(temp);
+}
+
+bool isSafe(int row , int col , vector< vector<int> > &board , int n){
+	int x = row;
+	int y = col;
+
+	// check for same row
+	while(y >= 0){
+		if(board[x][y] == 1){
+			return false;
+		}
+		y--;
+	}
+
+	x = row;
+	y = col;
+
+	// check for digonal
+	while(y >= 0 && x >=0){
+		if(board[x][y] == 1){
+			return false;
+		}
+		y--;
+		x--;
+	}
+
+	x = row;
+	y = col;
+
+	// check for digonal
+	while(x < n && y >= 0){
+		if(board[x][y] == 1){
+			return false;
+		}
+		y--;
+		x++;
+	}
+	return true;
+}
+
+void solve(int col , vector<vector<int> >&ans ,vector<vector<int> > &board , int n){
+	//base case
+	if(col == n){
+		addSolution(ans , board , n);
+		return;
+	}
+
+	// solve 1 case and recursion will solve
+	for(int row = 0 ; row < n ; row++){
+		if(isSafe(row , col , board , n)){
+			//if placing queen is safe
+			board[row][col] = 1;
+			solve(col + 1 , ans , board , n);
+			board[row][col] = 0;
+		}
+	}
+}
+
+vector<vector<int>> nQueens(int n)
+{
+	// Write your code here
+	vector< vector<int> > board(n , vector<int>(n , 0));
+	vector< vector<int> > ans;
+
+	solve(0 , ans , board , n);
+	return ans;
+
+}
